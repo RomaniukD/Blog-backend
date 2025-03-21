81b13cbe10fefe8f703da6be8d62451161c7de49
Denis <kubikirubiki228@gmail.com>
2025-03-21 23:45:54 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -27,10 +27,7 @@ const PostSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Comment',
     }],
-    imageUrl: {
-      type: Array,
-      default: [],
-    },
+    imageUrl:String,
   },
   {
     timestamps: true,
